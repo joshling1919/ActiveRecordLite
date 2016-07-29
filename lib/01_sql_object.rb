9e719a2926af0974a338404dec388038319fb891
Joshua Ling <joshling1919@gmail.com>
2016-07-29 15:01:25 -0700

    implemented insert method

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -35,15 +35,36 @@ class SQLObject
   end
 
   def self.all
-    # ...
+      all_rows = DBConnection.execute(<<-SQL)
+        SELECT
+          *
+        FROM
+          #{self.table_name}
+    SQL
+    self.parse_all(all_rows)
   end
 
   def self.parse_all(results)
-    # ...
+    final_array = []
+    results
+    results.each do |options_hash|
+      options_hash
+      final_array << self.new(options_hash)
+    end
+    final_array
   end
 
   def self.find(id)
-    # ...
+    p correct_row = DBConnection.execute(<<-SQL, id).first
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        id = ?
+    SQL
+
+    self.new(correct_row) if correct_row
   end
 
   def initialize(params = {})
@@ -52,7 +73,7 @@ class SQLObject
       if self.class.columns.include?(name_sym)
         self.send("#{name_sym}=", value)
       else
-        raise "unknown attribute '#{name_sym}'"
+        raise "unknown attribute '#{name}'"
       end
     end
   end
@@ -62,11 +83,32 @@ class SQLObject
   end
 
   def attribute_values
-    @attributes.values
+      @attributes.values
   end
 
   def insert
-    # ...
+    col = self.class.columns.drop(1)
+    self.class.columns
+    col_names = "(#{col.join(", ")})"
+    question_marks = "("
+      col.length.times do |index|
+        if index < col.length - 1
+          question_marks << "?, "
+        else
+          question_marks << "?)"
+        end
+      end
+    question_marks
+    *self.attribute_values
+    DBConnection.execute(<<-SQL, *self.attribute_values)
+      INSERT INTO
+        #{self.class.table_name} #{col_names}
+      VALUES
+        #{question_marks}
+    SQL
+
+    attributes[:id] = DBConnection.last_insert_row_id
+
   end
 
   def update
